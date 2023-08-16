6ff8c2c1d87108de0cd869d4389517698f65010e
Aayush2120 <85314084+Aayush2120@users.noreply.github.com>
2023-08-16 20:01:30 +0530

    Update demoapp12.c

--- demoapp12.c
+++ demoapp12.c
@@ -4,10 +4,10 @@
 Navigation nav;
 int main(int argc, char *argv[])
 {
-    nav.take_off(3.0);						//Taking Off
-    nav.position_set(5,0,-3);				//Sending Position Setpoints
-    nav.position_set(5,5,-3);
-    nav.position_set(0,5,-3);
-    nav.position_set(0,0,-3);
+    nav.take_off(5.0);						//Taking Off
+    nav.position_set(6.5,0,-5);				//Sending Position Setpoints
+    nav.position_set(6.5,6.5,-5);
+    nav.position_set(0,6.5,-5);
+    nav.position_set(0,0,-5);
     nav.land(false);									//Landing
 }
